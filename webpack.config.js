dd3b6f7d58218289a9ee897e584950acc6748a62
ArtemLavrentii <lavrentiyartem@gmail.com>
2020-02-03 14:41:49 +0200

    Added basic page with configs

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: 'index.html' }),
+  ],
+};
